7bdb1bc9767bde7761943a2c18e57b7bce0aa882
lucasquearis <lucasquearis@hotmail.com>
2022-01-16 22:38:45 -0300

    exercicios 1.2

---
+++ Exercícios/exercicio1-2.py
@@ -0,0 +1,12 @@
+# Quantos segundos há em 42 minutos e 42 segundos?
+
+print((42 * 60) + 42)
+
+# Quantas milhas há em 10 quilometros?
+
+print(10 * 1.61)
+
+# Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio? Qual é a sua velocidade média em milhas por hora?
+
+print((2562 / 60) / 10)
+print(16.1 / (((42 * 60) + 42) * 3600))
